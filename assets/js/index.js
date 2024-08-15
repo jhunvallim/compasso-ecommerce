56ad3b46cb19e8fa2ac0a01066e0cc72b1197753
Helio Vallim <h.vallim@gmail.com>
2024-08-14 23:54:37 -0300

    Feat: criação do carrossel de comentários

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,61 @@
-const discount = document.querySelectorAll('.product-card--discount');
+const discount = document.querySelectorAll(".product-card--discount");
 
-discount.forEach(div => {
-  if (div.textContent.trim() !== '') {
-    div.classList.add('discount-has-text');
+discount.forEach((div) => {
+  if (div.textContent.trim() !== "") {
+    div.classList.add("discount-has-text");
   }
 });
+
+// Comment Slider
+
+const arrowRight = document.querySelector(".arrow-right");
+const arrowLeft = document.querySelector(".arrow-left");
+let firstCard = document.querySelector(".card-area");
+
+
+const scrollAmount = 400;
+
+arrowLeft.addEventListener('click', (event) => {
+  event.preventDefault();
+  const currentPosition = firstCard.scrollLeft;
+  const targetPosition = currentPosition - scrollAmount;
+  firstCard.scrollTo({
+    left: targetPosition,
+    behavior: 'smooth'
+  });
+});
+
+arrowRight.addEventListener('click', (event) => {
+  event.preventDefault();
+  const currentPosition = firstCard.scrollLeft;
+  const targetPosition = currentPosition + scrollAmount;
+  const scrollableWidth = firstCard.scrollWidth - firstCard.clientWidth;
+
+  if (targetPosition > scrollableWidth) {
+    // Volta ao início ao atingir o limite
+    firstCard.scrollTo({
+      left: 0,
+      behavior: 'smooth'
+    });
+  } else {
+    firstCard.scrollTo({
+      left: targetPosition,
+      behavior: 'smooth'
+    });
+  }
+});
+
+
+let teste = document.querySelectorAll('.card-container');
+console.log(teste);
+
+//Função pra trocar divs
+
+// arrowRight.addEventListener("click", function() {
+//   var divs = document.querySelectorAll(".card-container");
+//   if (divs.length > 1) {
+//     var firstDiv = divs[0];
+//     var lastDiv = divs[divs.length - 1];
+//     lastDiv.parentNode.insertBefore(firstDiv, lastDiv.nextSibling);
+//   }
+// });
