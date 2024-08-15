6f4b2fe67f14299638423d39119f16e05bd7e7c3
Helio Vallim <h.vallim@gmail.com>
2024-08-15 17:54:50 -0300

    feat: criar a função para clonar o primeiro card

--- assets/js/functions/comments-carousel.js
+++ assets/js/functions/comments-carousel.js
@@ -1,60 +1,67 @@
-
+//Comment Clone
+function commentClone() {
+  const cardContainer = document.querySelector(".card-area");
+  const firstCard = cardContainer.firstElementChild;
+  const clone = firstCard.cloneNode(true); // Clona o elemento e seus filhos
+  clone.style.marginLeft = "0px";
+  cardContainer.appendChild(clone);
+}
 
 // Comment Slider
-export function commentsCarousel() {
-const arrowRight = document.querySelector(".arrow-right");
-const arrowLeft = document.querySelector(".arrow-left");
-let cardArea = document.querySelector(".card-area");
+function commentsCarousel() {
+  const arrowRight = document.querySelector(".arrow-right");
+  const arrowLeft = document.querySelector(".arrow-left");
+  const cardArea = document.querySelector(".card-area");
 
+  const scrollAmount = 420;
 
-const scrollAmount = 420;
-
-arrowLeft.addEventListener('click', (event) => {
-  event.preventDefault();
-  const currentPosition = cardArea.scrollLeft;
-  const targetPosition = currentPosition - scrollAmount;
-  cardArea.scrollTo({
-    left: targetPosition,
-    behavior: 'smooth'
-  });
-});
-
- arrowRight.addEventListener('click', (event) => {
-  //  event.preventDefault();
-   const currentPosition = cardArea.scrollLeft;
-   const targetPosition = currentPosition + scrollAmount;
-   const scrollableWidth = cardArea.scrollWidth - cardArea.clientWidth;
-
-   if (targetPosition > scrollableWidth) {
-    //Volta ao início
-    //  cardArea.scrollTo({
-    //    left: 0,
-    //    behavior: 'smooth'
-    //  });
-    const animationDuration = 500; 
-    const easeType = 'ease'; // Função de easing (pode ser 'ease-in', 'ease-out', etc.)
-
+  arrowLeft.addEventListener("click", (event) => {
+    event.preventDefault();
+    const currentPosition = cardArea.scrollLeft;
+    const targetPosition = currentPosition - scrollAmount;
     cardArea.scrollTo({
-      left: 0,
-      behavior: 'smooth'
+      left: targetPosition,
+      behavior: "smooth",
     });
+  });
 
-    // Transição
-    cardArea.style.transition = `transform ${animationDuration}ms ${easeType}`;
-    cardArea.style.transform = 'rotateY(360deg)';
+  arrowRight.addEventListener("click", (event) => {
+    //  event.preventDefault();
+    const currentPosition = cardArea.scrollLeft;
+    const targetPosition = currentPosition + scrollAmount;
+    const scrollableWidth = cardArea.scrollWidth - cardArea.clientWidth;
 
-    // Limpa transição
-    setTimeout(() => {
-      cardArea.style.transition = '';
-      cardArea.style.transform = '';
-    }, animationDuration * 2);
-   } else {
-     cardArea.scrollTo({
-       left: targetPosition,
-       behavior: 'smooth'
-     });
-   }
- });
+    if (targetPosition > scrollableWidth) {
+      //Volta ao início
+      const animationDuration = 500;
+      const easeType = "ease-in-out"; 
+
+      cardArea.scrollTo({
+        left: 0,
+        behavior: "smooth",
+      });
+
+      // Transição
+      cardArea.style.transition = `all ${animationDuration}ms ${easeType}`;
+      
+      cardArea.style.transform = "rotateY(-360deg)";
+      cardArea.style.opacity = "0";
+      setTimeout(() => {
+        cardArea.style.opacity = 1;
+      
+        // Limpa transição após a animação
+        setTimeout(() => {
+          cardArea.style.transition = "";
+          cardArea.style.transform = "";
+        }, animationDuration);
+      }, 200);
+    } else {
+      cardArea.scrollTo({
+        left: targetPosition,
+        behavior: "smooth",
+      });
+    }
+  });
 }
 
 // arrowRight.addEventListener('click', (event) => {
@@ -90,16 +97,16 @@ arrowLeft.addEventListener('click', (event) => {
 //   }
 // });
 
-
-let teste = document.querySelectorAll('.card-container');
+let teste = document.querySelectorAll(".card-cardContainer");
 console.log(teste);
 
-
 // arrowRight.addEventListener("click", function() {
-//   var divs = document.querySelectorAll(".card-container");
+//   var divs = document.querySelectorAll(".card-cardContainer");
 //   if (divs.length > 1) {
 //     var firstDiv = divs[0];
 //     var lastDiv = divs[divs.length - 1];
 //     lastDiv.parentNode.insertBefore(firstDiv, lastDiv.nextSibling);
 //   }
 // });
+
+export { commentClone, commentsCarousel };
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@
 import { discountStyle } from "./functions/discount-style.js";
 import { closeModal } from "./functions/close-modal.js";
 import { commentsCarousel } from "./functions/comments-carousel.js";
+import { commentClone } from "./functions/comments-carousel.js";
 
 //Run imports
 document.addEventListener("DOMContentLoaded", discountStyle());
 document.addEventListener("DOMContentLoaded", closeModal());
+document.addEventListener("DOMContentLoaded", commentClone());
 document.addEventListener("DOMContentLoaded", commentsCarousel());
 
