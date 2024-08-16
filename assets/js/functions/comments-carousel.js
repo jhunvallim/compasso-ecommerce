7c9320457acc4cd8403efa7687fa3a14e42566dd
Helio Vallim <h.vallim@gmail.com>
2024-08-16 15:54:59 -0300

    feat: criação do carrosel e da animação dele

--- assets/js/functions/comments-carousel.js
+++ assets/js/functions/comments-carousel.js
@@ -4,6 +4,7 @@ function commentClone() {
   const firstCard = cardContainer.firstElementChild;
   const clone = firstCard.cloneNode(true); // Clona o elemento e seus filhos
   clone.style.marginLeft = "0px";
+  clone.classList.add("copy-card");
   cardContainer.appendChild(clone);
 }
 
@@ -14,15 +15,38 @@ function commentsCarousel() {
   const cardArea = document.querySelector(".card-area");
 
   const scrollAmount = 420;
+  
+      const animationDuration = 500;
+      const easeType = "ease-in-out"; 
 
   arrowLeft.addEventListener("click", (event) => {
     event.preventDefault();
     const currentPosition = cardArea.scrollLeft;
-    const targetPosition = currentPosition - scrollAmount;
+    const targetPosition = currentPosition - scrollAmount - 20;
+    if (currentPosition === 0) {
+      //Avança até o final
+      cardArea.scrollTo({
+        left: 850,
+        behavior: "smooth" 
+      })
+      cardArea.style.transition = `all ${animationDuration}ms ${easeType}`;
+      
+      cardArea.style.transform = "rotateY(-360deg)";
+      cardArea.style.opacity = "0";
+      setTimeout(() => {
+        cardArea.style.opacity = 1;
+      
+        // Limpa transição
+        setTimeout(() => {
+          cardArea.style.transition = "";
+          cardArea.style.transform = "";
+        }, animationDuration);
+      }, 200);
+    } else {
     cardArea.scrollTo({
       left: targetPosition,
       behavior: "smooth",
-    });
+    });}
   });
 
   arrowRight.addEventListener("click", (event) => {
@@ -34,8 +58,6 @@ function commentsCarousel() {
 
     if (targetPosition > scrollableWidth) {
       //Volta ao início
-      const animationDuration = 500;
-      const easeType = "ease-in-out"; 
 
       cardArea.scrollTo({
         left: 0,
@@ -50,7 +72,7 @@ function commentsCarousel() {
       setTimeout(() => {
         cardArea.style.opacity = 1;
       
-        // Limpa transição após a animação
+        // Limpa transição 
         setTimeout(() => {
           cardArea.style.transition = "";
           cardArea.style.transform = "";
@@ -64,50 +86,5 @@ function commentsCarousel() {
     }
   });
 }
-
-// arrowRight.addEventListener('click', (event) => {
-//   event.preventDefault();
-//   const currentPosition = cardArea.scrollLeft;
-//   const targetPosition = currentPosition + scrollAmount;
-//   const scrollableWidth = cardArea.scrollWidth - cardArea.clientWidth;
-
-//   if (targetPosition > scrollableWidth) {
-//     // Voltar ao início com efeito de carrossel
-//     const animationDuration = 1000; // Duração da animação em milissegundos
-//     const easeType = 'ease-out'; // Função de easing (pode ser 'ease-in', 'ease-out', etc.)
-
-//     cardArea.scrollTo({
-//       left: 0,
-//       behavior: 'smooth'
-//     });
-
-//     // Adicionar a transição suave
-//     cardArea.style.transition = `transform ${animationDuration}ms ${easeType}`;
-//     cardArea.style.transform = 'rotate3d(1, 2, 100, 360deg)';
-
-//     // Limpar a transição após a animação
-//     setTimeout(() => {
-//       cardArea.style.transition = '';
-//       cardArea.style.transform = '';
-//     }, animationDuration);
-//   } else {
-//     cardArea.scrollTo({
-//       left: targetPosition,
-//       behavior: 'smooth'
-//     });
-//   }
-// });
-
-let teste = document.querySelectorAll(".card-cardContainer");
-console.log(teste);
-
-// arrowRight.addEventListener("click", function() {
-//   var divs = document.querySelectorAll(".card-cardContainer");
-//   if (divs.length > 1) {
-//     var firstDiv = divs[0];
-//     var lastDiv = divs[divs.length - 1];
-//     lastDiv.parentNode.insertBefore(firstDiv, lastDiv.nextSibling);
-//   }
-// });
 
 export { commentClone, commentsCarousel };
---
+++ assets/js/functions/teste.js
@@ -0,0 +1,11 @@
+const arrowRight = document.querySelector(".arrow-right");
+const arrowLeft = document.querySelector(".arrow-left");
+const cardArea = document.querySelector(".card-area");
+
+
+// arrowRight.addEventListener("click", () => {
+//   cardArea.scrollTo({
+//     left: cardArea.lastElementChild.offsetLeft + 1600,
+//     behavior: "smooth",
+//   });
+// });
